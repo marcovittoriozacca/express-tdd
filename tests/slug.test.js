3d6bbb0dea4d1ecf7ab1c1319918a0d41e1ed21e
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-30 15:45:58 +0200

    feat: error when array is missing. Fix: for slug -1

--- tests/slug.test.js
+++ tests/slug.test.js
@@ -1,15 +1,15 @@
 const {test, expect} = require('@jest/globals');
-
+const postsList = require('../db/posts.json');
 //tasks: 
 // createSlug dovrebbe ritornare una stringa ✓
 // createSlug dovrebbe ritornare una stringa in lowercase ✓
 // createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da - ✓
-// createSlug dovrebbe incrementare di 1 lo slug quando esiste già
-// createSlug dovrebbe lanciare un errore in caso di titolo non presente o formato errato
-// createSlug dovrebbe lanciare un errore se manca l'array dei post
+// createSlug dovrebbe incrementare di 1 lo slug quando esiste già ✓
+// createSlug dovrebbe lanciare un errore in caso di titolo non presente o formato errato ✓
+// createSlug dovrebbe lanciare un errore se manca l'array dei post ✓
 
-const  createSlug = (string) => {
-    if(!string){
+const  createSlug = (string, posts) => {
+    if(!string || !posts){
         throw new Error();
     }
 
@@ -25,10 +25,9 @@ const  createSlug = (string) => {
                 .replaceAll(" ", "-")
                 .toLowerCase();
     
-    const slugsList = ["hello", "hello-world"];
     let baseSlug = string;
     let counter = 1;
-
+    const slugsList = posts.map(e => e.slug);
     while(slugsList.includes(baseSlug)){
         baseSlug = `${string}-${counter}`;
         counter++;
@@ -40,14 +39,14 @@ const  createSlug = (string) => {
 
 //createSlug returns a string
 test('createSlug dovrebbe ritornare una stringa', () => {
-
-    expect( typeof createSlug(12) ).toBe("string");
+    const slug = createSlug(12, postsList);
+    expect( typeof slug).toBe("string");
 })
 
 
 //createSlug retuns a lower case string
 test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
-    const slug = createSlug("Hello WORLD");
+    const slug = createSlug("Hello WORLD", postsList);
 
     expect( slug ).toEqual(slug.toLowerCase());
 })
@@ -55,14 +54,15 @@ test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
 
 //createSlug must replace all white spaces with -
 test('createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da -', () => {
-    const slug = createSlug("Hello World");
+    const slug = createSlug("Hello World", postsList);
 
     expect(slug).toEqual(slug.replaceAll(" ", "-"));
 })
 
 test('createSlug dovrebbe incrementare di 1 lo slug quando esiste già', () => {
-    const slug = createSlug("Hello");
-    expect(slug).toBe("hello-1");
+    const slugsList = postsList.map(p => p.slug)
+    const slug = createSlug("ciambellone", postsList);
+    expect(slugsList.includes(slug)).toBe(false);
 })
 
 
@@ -70,5 +70,11 @@ test('createSlug dovrebbe lanciare un errore in caso di titolo non presente o fo
     
     expect(() => createSlug() ).toThrowError();
     expect(() => createSlug(undefined) ).toThrowError();
+    expect(() => createSlug(undefined, posts) ).toThrowError();
     expect(() => createSlug("/") ).toThrowError();
+})
+
+
+test("createSlug dovrebbe lanciare un errore se manca l'array dei post", () => {
+    expect( () => createSlug("slug", undefined) ).toThrowError();
 })
