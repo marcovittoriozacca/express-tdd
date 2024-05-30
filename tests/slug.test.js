3b066bc6f42a1df4a37fad1bde6cabeb4a63efe0
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-30 14:49:43 +0200

    fix: replace white spaces with -

--- tests/slug.test.js
+++ tests/slug.test.js
@@ -1,20 +1,21 @@
 const {test, expect} = require('@jest/globals');
 
 //tasks: 
-// createSlug dovrebbe ritornare una stringa
-// createSlug dovrebbe ritornare una stringa in lowercase
-// createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da -
+// createSlug dovrebbe ritornare una stringa ✓
+// createSlug dovrebbe ritornare una stringa in lowercase ✓
+// createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da - ✓
 // createSlug dovrebbe incrementare di 1 lo slug quando esiste già
 // createSlug dovrebbe lanciare un errore in caso di titolo non presente o formato errato
 // createSlug dovrebbe lanciare un errore se manca l'array dei post
 
 const  createSlug = (string) => {
+    
     let slug = string;
     if(typeof string !== "string"){
         slug = string+"";
     }
     slug = slug
-            .replaceAll("/", "-")
+            .replaceAll(" ", "-")
             .toLowerCase();
 
 
@@ -37,9 +38,9 @@ test('createSlug dovrebbe ritornare una stringa in lowercase', () => {
 })
 
 
-//createSlug must replace all / with -
+//createSlug must replace all white spaces with -
 test('createSlug dovrebbe ritornare una stringa con gli spazi sostituiti da -', () => {
-    const slug = createSlug("Hello/World");
+    const slug = createSlug("Hello World");
 
-    expect(slug).toEqual(slug.replaceAll("/", "-"));
+    expect(slug).toEqual(slug.replaceAll(" ", "-"));
 })
