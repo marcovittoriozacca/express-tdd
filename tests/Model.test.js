20e7a7312850934faf79e68c57d3007f5f9e76fa
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-30 16:40:13 +0200

    feat: BONUS model test

--- tests/Model.test.js
+++ tests/Model.test.js
@@ -1,24 +1,60 @@
 const {test, expect} = require('@jest/globals');
 // Model dovrebbe essere una classe (crea un'istanza della classe Model) ✓
-// L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza
-// L'istanza di model dovrebbe avere il metodo read
-// L'istanza di model dovrebbe avere il metodo add
-// read dovrebbe ritornare un array
+// L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza ✓
+// L'istanza di model dovrebbe avere il metodo read ✓
+// L'istanza di model dovrebbe avere il metodo add ✓
+// read dovrebbe ritornare un array ✓
 // add dovrebbe aggiungere un elemento all’array dei dati e ritornare tutta la lista
 
 class Model {
+    data = [1,2,3,4,5];
     constructor(filename){
+        if(!filename){
+            throw new Error
+        }
         this.filename = filename;
     }
-}
 
+    read(){
+        return this.data;
+    }
+    add(value){
+        if(!value){
+            throw new Error;
+        }else{
+            this.data.push(value)
+            return this.read;
+        }
+    }
+}
 test("Model dovrebbe essere una classe (crea un'istanza della classe Model)", () => {
+    const model = new Model("filename");
     expect(Model).toBeDefined();
-    expect(typeof Model).toBe("function");
+    expect(model).toBeInstanceOf(Model);
 })
 
 test("L'istanza di model dovrebbe richiedere il nome del file json da usare in fase di creazione dell'istanza", () => {
     const model = new Model("filename");
     expect(model.filename).toBeDefined();
+})
+
+test("L'istanza di model dovrebbe avere il metodo read", () => {
+    const model = new Model("filename");
+    expect(model.read).toBeDefined();
+})
+
+test("L'istanza di model dovrebbe avere il metodo add", () => {
+    const model = new Model("filename");
+    expect(model.add).toBeDefined();
+})
+
+test("read dovrebbe ritornare un array", () => {
+    const model = new Model("filename");
+    expect( Array.isArray(model.read()) ).toBe(true);
+})
+
+test("add dovrebbe aggiungere un elemento all’array dei dati e ritornare tutta la lista", () => {
+    const model = new Model("filename");
+    expect( () => model.add(undefined) ).toThrowError();
 })
 
